57cdb9228505ea4e3a28c0acedbb81afe5ac9e25
Jeeva <famousjeeva@gmail.com>
2023-11-21 11:28:48 +0530

    Success

---
+++ src/test/java/com/onesoft/devapp/DevappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onesoft.devapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
